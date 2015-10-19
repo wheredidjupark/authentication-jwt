49d11a49667edb2677da4931dbb08518d494c5a4
Ju Hyeon Park <wheredidjupark@gmail.com>
2015-10-19 14:37:11 -0700

    used the authToken service to hide register button if authenticated

---
+++ client/app/scripts/controllers/headerController.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angular.module('psJWTApp')
+  .controller('HeaderController', function ($scope, authToken) {
+
+  	$scope.isAuthenticated = authToken.isAuthenticated;
+  });
--- client/app/scripts/controllers/registerController.js
+++ client/app/scripts/controllers/registerController.js
@@ -2,7 +2,7 @@
 
 (function() {
 
-    angular.module("psJWTApp").controller('RegisterController', function($scope, $http, alert) {
+    angular.module("psJWTApp").controller('RegisterController', function($scope, $http, alert, authToken) {
         $scope.email = "";
         $scope.password = "";
         $scope.passwordConfirm = "";
@@ -18,10 +18,10 @@
             console.log("you are attempting to submit a registration form with following email: ", user.email);
             $http.post(url, user).then(function(response) {
                 alert('Success', 'OK!', 'You are now registered');
-                console.log(response);
+                authToken.setToken(response.token);
+
             }, function(error) {
                 alert('warning', 'Error', error.data);
-                console.log(error);
             });
         };
 
